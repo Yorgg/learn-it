8e3652dd6569981fbd985a4cdb5f6ae2d71b22da
Jon <jyorg@outlook.com>
2015-06-17 23:30:48 -0700

    Add 'complete' and 'remove' icons to user page - with ajax implementation'

--- app/assets/javascripts/users.coffee
+++ app/assets/javascripts/users.coffee
@@ -1,3 +1,12 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).on "page:change", ->
+  $('div[id^="task-"]').hover( ->
+    item = $(this)        
+    item.find('.icons').show()
+  ,->
+    $(this).find('.icons').hide())
+ 
+ 
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,6 +13,26 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = current_user.tasks.find(params[:id])
+    @task.complete = !(@task.complete)
+    if @task.save      
+      respond_to do |format|
+        format.html {redirect_to user_path(current_user.id)}
+        format.js { render :action => "complete.js.erb" }
+      end 
+    end
+  end
+
+  def destroy
+    @task = current_user.tasks.find(params[:id])
+    @task.delete
+    respond_to do |format|
+        format.html {redirect_to user_path(current_user.id)}
+        format.js { render :action => "delete.js.erb" }
+    end 
+  end
+
   private
 
   def task_params
